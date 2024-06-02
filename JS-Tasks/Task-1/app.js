508d81a39b752cee03174561ebada46bccf0b8a8
Zubair Ahmed <STr1k3R768@gmail.com>
2024-06-02 13:48:08 +0500

    JS Task-1 Even/Odd

--- JS-Tasks/Task-1/app.js
+++ JS-Tasks/Task-1/app.js
@@ -1,28 +1,42 @@
 function myFunction() {
-    const numArray = [1, 2, 3, 4, 5 ];
+    const numArray = [1, 2, 3, 4, 5];
     const myNumber = document.getElementById("myNumber").value;
     // const evens = numArray.filter(num => num % 2 === 0);
     
-    if (myNumber.length == 0) {
-        alert("Please enter a number");
+    // if(isNaN(myNumber) || myNumber <=0 ){
+    //     alert("Please enter a valid number");
+    //     return;
+    // }
+
+    if (isNaN(myNumber) || myNumber <= 0) {
+        // alert("Please enter a valid number");
+        document.getElementById("myNum").innerHTML ="Please enter a valid number";
+        document.getElementById('myNum').style.color = "#f00";
+        
     } else {
         numArray.push(myNumber)
 
         let even = [];
         let odd = [];
+        let evenCount = 0
+        let oddCount = 0
 
         for (let i = 0; i < numArray.length; i++) {
             if (numArray[i] % 2 === 0) {
                 even.push(numArray[i]);
+                evenCount++;
             } else {
                 odd.push(numArray[i]);
+                oddCount++;
             }
         }
 
         if (myNumber % 2 === 0) {
             document.getElementById("myNum").innerHTML ="The number you entered is even";
+            document.getElementById('myNum').style.color = "#000";
         } else {
             document.getElementById("myNum").innerHTML = "The number you entered is odd";
+            document.getElementById('myNum').style.color = "#000";
         }
 
         console.log("Numbers are: " + numArray);
@@ -32,5 +46,8 @@ function myFunction() {
         document.getElementById("mixNumbers").innerHTML = numArray;
         document.getElementById("evenNumbers").innerHTML = even;
         document.getElementById("oddNumbers").innerHTML = odd;
+        document.getElementById("numberCounter").innerHTML = numArray.length;
+        document.getElementById("evenCounter").innerHTML = evenCount;
+        document.getElementById("oddCounter").innerHTML = oddCount;
     }
 }
